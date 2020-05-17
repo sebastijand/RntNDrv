6b236dfece53fd79fc76f5d4a77179ca308c4a2f
sebastijand <58041623+sebastijand@users.noreply.github.com>
2020-05-17 16:22:47 +0200

    commit 6

--- frontend/src/views/Klasa vozila.vue
+++ frontend/src/views/Klasa vozila.vue
@@ -2,8 +2,8 @@
   <div class="klasa">
     <h1 style="padding: 20px; color: #2c3e50">Klasa</h1>
 
-    <div style="float: left; margin: 20px; width: 300px">
-      <img src="škoda.png" style="width: 250px">
+    <div class="izbornik1" style="float: left; padding: 20px; width: 300px">
+      <img src="vw up transparent.png" style="width: 250px">
       <h1>Mini</h1> 
       <!--
       <router-link to="/model" sytle="float: left">
@@ -12,15 +12,15 @@
       -->
     </div>
     
-    <div style="float: left; margin: 10px; width: 300px">
-      <img src="audi.jpg" style="width: 250px">
+    <div class="izbornik1" style="float: left; padding: 20px; width: 300px">
+      <img src="audi transparent.png" style="width: 250px">
       <h1>Sedan</h1> 
       <router-link to="/model" sytle="float: left">
         <button type="file" class="btn btn-primary mt-5" style="width: 100px">Odaberi</button>
       </router-link> 
     </div>
     
-    <div style="float: left; margin: 10px; width: 300px">
+    <div class="izbornik1" style="float: left; padding: 20px; width: 300px">
       <img src="opel-vivaro-png.png" style="width: 250px">
       <h1>Kombi</h1> 
       <!--
@@ -30,7 +30,7 @@
       -->
     </div>
     
-    <div class="izbornik1" style="float: left; margin: 10px; width: 300px"> <!-- <div class="klasa1" style="float: left; margin: 10px"> -->    
+    <div class="izbornik1" style="float: left; margin: 10px; width: 200px"> <!-- <div class="klasa1" style="float: left; margin: 10px"> -->    
       <router-link to="/uređivanjeprofila">
         <p>Prije odabira vozila, potrebno je nadopuniti vlastiti profil</p>
       </router-link>
@@ -41,15 +41,14 @@
 
 <style scoped>
   .izbornik1 {
-    /*margin: 151px;*/
-    padding: 15px;
+    display: inline-block;
+    margin: 15px;
+    padding: 40px;
     border-style: inset;
     border-width: 3px;
-    border-color: rgba(1, 235, 252, 0.575);
+    border-color: rgb(80, 77, 39);
     border-radius: 5px;
-    /*background-color: #f1f1f1;*/
-    background: rgb(255,255,255);
-    background: linear-gradient(50deg, rgba(255,255,255,1) 0%, rgba(196,231,227,1) 100%);
+    background-color: #f1f1f1;
   } 
   /*
   #container{width:100%;}
